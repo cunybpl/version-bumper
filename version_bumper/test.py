e6d3c7218c2ddf3a0b560e27f9c36e35dbf403fa
Kuba Gasiorowski <kgasiorowski123@gmail.com>
2024-08-15 14:28:33 -0400

    Initial commit

---
+++ version_bumper/test.py
@@ -0,0 +1,19 @@
+from cleo.commands.command import Command
+from poetry.plugins.application_plugin import ApplicationPlugin
+
+
+class CustomCommand(Command):
+    name = "test"
+
+    def handle(self) -> int:
+        self.line("My command")
+        return 0
+
+
+def factory() -> "CustomCommand":
+    return CustomCommand()
+
+
+class MyApplicationPlugin(ApplicationPlugin):
+    def activate(self, application):
+        application.command_loader.register_factory("test", factory)
